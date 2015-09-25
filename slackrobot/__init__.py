66191040e21ce27778fef5627d9f67b16d10fe6b
Ricky Winata <rwinata@traveloka.com>
2015-09-25 18:02:39 +0700

    refactor code to follow pylint analysis

--- slackrobot/__init__.py
+++ slackrobot/__init__.py
@@ -1,13 +1,26 @@
+"""
+slack-robot, simple slack bot implementation
+"""
 import json
 import logging
 import re
 import time
 import slackrobot.utils.plugins
+from ssl import SSLError
 from collections import OrderedDict
 from slackclient import SlackClient
+from slackclient._client import SlackNotConnected
 from slackrobot.utils.thread import ThreadPoolExecutor as Pool
 
 class SlackRobot(object):
+    """SlackRobot implementation
+
+    Public attributes:
+    - slack_client: for accessing slack api (SlackClient)
+    - bot_id: bot id in slack, exist when bot connected to slack (string)
+    - bot_name: bot name in slack, exist when bot connected to slack (string)
+    """
+
     def __init__(self, token, max_workers=8):
         self.bot_id = None
         self.bot_name = None
@@ -15,87 +28,123 @@ class SlackRobot(object):
         self.cron_plugins = []
         self.plugins = []
         self.slack_client = SlackClient(token)
-        self._load_plugins()
-        self._executor = Pool(max_workers=max_workers)
-    
-    def _load_plugins(self):
+        self.load_plugins()
+        self.executor = Pool(max_workers=max_workers)
+
+    def load_plugins(self):
+        """Load plugins (normal & cron)"""
         # load cron plugins
-        for cron_plugin in slackrobot.utils.plugins.cron_loader.all():
+        for cron_plugin in slackrobot.utils.plugins.CRON_LOADER.all():
             self.cron_plugins.append(cron_plugin)
         # load plugins
-        for plugin in slackrobot.utils.plugins.loader.all():
-            self.plugins.append(plugin) 
- 
+        for plugin in slackrobot.utils.plugins.LOADER.all():
+            self.plugins.append(plugin)
+
     def connect(self):
+        """Connect to slack server"""
         logging.info('Trying to connect with Slack...')
         self.slack_client.rtm_connect()
         self.bot_id = self.slack_client.server.login_data['self']['id']
         self.bot_name = self.slack_client.server.login_data['self']['name']
-        logging.info('Connected with bot name @{}'.format(self.bot_name))
+        logging.info('Connected with bot name @%s', self.bot_name)
 
     def start(self):
+        """Start the bot"""
         self.connect()
         while True:
             self.try_read_input()
             self.crons()
             self.autoping()
             time.sleep(.1)
-    
+
     def try_read_input(self):
+        """Try to read inputs from rtm channel"""
         try:
             for reply in self.slack_client.rtm_read():
                 self.input(reply)
-        except Exception as e:
-            logging.warning('Exception: {}'.format(str(e)))
+        except SlackNotConnected:
+            logging.warning('Slack is not connected.')
+        except SSLError, exception:
+            logging.warning('SSL Error: %s', exception)
 
     def autoping(self):
+        """Ping slack server, to ensure it's connected"""
         now = int(time.time())
         if now > self.last_ping + 3:
             self.slack_client.server.ping()
             self.last_ping = now
-    
-    def input(self, data):
-        # handle incoming messages
-        # handle only message type
-        if "type" in data and not "subtype" in data and data['type'] == 'message':            
+
+    def input(self, event):
+        """Handle incoming event to bot
+        only handle event with type message
+
+        Args:
+            event: event sent by slack when something happens.
+                for more information about the structure: https://api.slack.com/rtm
+        """
+        if "type" in event and not "subtype" in event and event['type'] == 'message':
             # handle message only if the message mentions the bot or
             # the channel is a direct message to the bot and
-            # don't handle messages that come from the bot itself
-            if ((data['text'].find('<@{}>'.format(self.bot_id)) != -1 or 
-                data['channel'].startswith('D')) and
-                data['user'] != self.bot_id):
-                # remove mention from message, get only the command
-                data['text'] = re.match('(<@.+>:?\s?)?(.+)', data['text']).group(2)
-                logging.info('got {}'.format(data))
-                for plugin in self.plugins: 
-                    self._executor.submit(plugin.do_job, self, data)
+            # don't handle the message coming from the bot itself
+            if ((event['text'].find('<@{}>'.format(self.bot_id)) != -1 or
+                 event['channel'].startswith('D')) and
+                    event['user'] != self.bot_id):
+                # remove mention (@bot_name) from message
+                event['text'] = re.match(r'(<@.+>:?\s?)?(.+)', event['text']).group(2)
+                for plugin in self.plugins:
+                    self.executor.submit(plugin.do_job, self, event)
 
     def crons(self):
+        """Run cron plugins job"""
         for cron_plugin in self.cron_plugins:
             if cron_plugin.interval + cron_plugin.last_run <= time.time():
                 cron_plugin.last_run = time.time()
-                self._executor.submit(cron_plugin.do_job, self)
-    
-    def send_message(self, text, channel, attachments=[]):
+                self.executor.submit(cron_plugin.do_job, self)
+
+    def send_message(self, text, channel, attachments=None):
+        """Send text message to slack channel
+
+        Args:
+            text: the text to be sent (string)
+            channel: what channel should the text be sent (string, ex: #test, #abc)
+            attachments: https://api.slack.com/docs/attachments
+
+        Return:
+            the message response returned by slack api.
+            for more information https://api.slack.com/methods/chat.postMessage
+        """
+        attachments = attachments or []
         send_data = {
             'text': text,
             'channel': channel,
             'attachments': json.dumps(attachments),
-            'as_user': True }
-        logging.info('Send message {} ...'.format(send_data))
+            'as_user': True}
+        logging.info('Send message %s ...', send_data)
         message = json.loads(self.slack_client.api_call('chat.postMessage', **send_data))
         if not message['ok']:
             logging.info('send_message failed: ' + message['error'])
         return message
 
     def update_message(self, text, message):
-       if message['ok']:
-            update_data = {
-                'text': text,
-                'ts': message['ts'],
-                'channel': message['channel'] }
-            logging.info('Update message {} ...'.format(update_data))
-            new_message = json.loads(self.slack_client.api_call('chat.update', **update_data))
-            if not new_message['ok']:
-                logging.info('update_message failed: ' + message['error'])
-            return new_message
+        """Update text message in sent message
+        Update only if the message['ok'] is True
+
+        Args:
+            text: updated text (string)
+            message: the message that want to be changed, got from method send_message
+                for more information: https://api.slack.com/methods/chat.postMessage
+
+        Return:
+            the message response returned by slack api.
+            for more information: https://api.slack.com/methods/chat.postMessage
+        """
+        update_data = {
+            'text': text,
+            'ts': message['ts'],
+            'channel': message['channel']}
+        logging.info('Update message %s ...', update_data)
+        new_message = json.loads(self.slack_client.api_call('chat.update', **update_data))
+        if not new_message['ok']:
+            logging.info('update_message failed: %s', message['error'])
+        return new_message
+
--- slackrobot/utils/plugins.py
+++ slackrobot/utils/plugins.py
@@ -1,35 +1,50 @@
+"""
+plugin utils for slack-robot
+"""
 import imp
 import glob
 import sys
 import os
-import time
 
-class Plugin(object):
+class Plugin(object): #pylint: disable-msg=R0903
+    """Represents normal plugin"""
     def __init__(self, func):
         self.func = func
-    
+
     def do_job(self, *args, **kwargs):
+        """run the job"""
         self.func(*args, **kwargs)
 
-class CronPlugin(object):
+class CronPlugin(object): #pylint: disable-msg=R0903
+    """Represents cron plugin"""
     def __init__(self, func, interval):
         self.func = func
         self.interval = interval
         self.last_run = 0
-    
+
     def do_job(self, *args, **kwargs):
+        """run the job"""
         self.func(*args, **kwargs)
 
 # Plugin loader
-class PluginLoader(object):
+class PluginLoader(object): #pylint: disable-msg=R0903
+    """
+    A loader for loading normal plugins to slack bot
+    """
     def __init__(self, func_name, plugins_path):
         self.func_name = func_name
         self.plugins_path = plugins_path
         sys.path.append(self.plugins_path)
-          
+
     def all(self):
+        """Get all normal plugins
+
+        Returns:
+            generator of all normal plugins
+        """
+
         for path in glob.glob(self.plugins_path+'/*.py'):
-            name, ext = os.path.splitext(os.path.basename(path))
+            name, _ = os.path.splitext(os.path.basename(path))
             if name.startswith('__'):
                 continue
             module = imp.load_source(name, path)
@@ -37,16 +52,22 @@ class PluginLoader(object):
                 yield Plugin(getattr(module, self.func_name))
 
 # Cron Plugin Loader
-class CronPluginLoader(PluginLoader):
+class CronPluginLoader(PluginLoader): #pylint: disable-msg=R0903
+    """
+    A loader for loading all cron plugins to slack bot
+    """
     def __init__(self, func_name, interval_name, plugins_path):
-        self.func_name = func_name
+        super(CronPluginLoader, self).__init__(func_name, plugins_path)
         self.interval_name = interval_name
-        self.plugins_path = plugins_path
-        sys.path.append(self.plugins_path)
 
     def all(self):
+        """Get all cron plugins
+
+        Returns:
+            generator of cron plugins
+        """
         for path in glob.glob(self.plugins_path+'/*.py'):
-            name, ext = os.path.splitext(os.path.basename(path))
+            name, _ = os.path.splitext(os.path.basename(path))
             if name.startswith('__'):
                 continue
             module = imp.load_source(name, path)
@@ -54,12 +75,12 @@ class CronPluginLoader(PluginLoader):
                 yield CronPlugin(getattr(module, self.func_name),
                                  getattr(module, self.interval_name))
 
-loader = PluginLoader(
+LOADER = PluginLoader(
     'process_message',
     './plugins'
 )
 
-cron_loader = CronPluginLoader(
+CRON_LOADER = CronPluginLoader(
     'cron_job',
     'cron_interval',
     './plugins'
